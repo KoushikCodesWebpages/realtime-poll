4ada3fe1a5a9fa3972479ce7ff60c9dd02a68c4b
koush <koushikbabu003@gmail.com>
2026-02-14 20:34:39 +0530

    setup dev and prod env

--- config/config-env.go
+++ config/config-env.go
@@ -7,19 +7,42 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func exists(name string) bool {
+	_, err := os.Stat(name)
+	return err == nil
+}
+
 func LoadEnv() error {
 
-	env := os.Getenv("APP_ENV")
-	if env == "" {
-		env = "dev"
+	// // 1️⃣ If APP_ENV is provided externally → respect it
+	// env := os.Getenv("APP_ENV")
+
+	// if env != "" {
+	// 	file := "."+ env + ".env." 
+	// 	if err := godotenv.Load(file); err != nil {
+	// 		return err
+	// 	}
+	// 	log.Println("Loaded environment from:", file)
+	// 	return nil
+	// }
+
+	// 2️⃣ Otherwise auto-pick file
+	if exists(".dev.env") {
+		if err := godotenv.Load(".dev.env"); err != nil {
+			return err
+		}
+		log.Println("Loaded environment: dev (auto)")
+		return nil
 	}
 
-	file := ".env." + env
-
-	if err := godotenv.Load(file); err != nil {
-		return err
+	if exists(".prod.env") {
+		if err := godotenv.Load(".prod.env"); err != nil {
+			return err
+		}
+		log.Println("Loaded environment: prod (auto)")
+		return nil
 	}
 
-	log.Println("Loaded environment:", env)
+	log.Fatal("No environment file found (.env.dev or .env.prod)")
 	return nil
 }
